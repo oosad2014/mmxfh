02fde74a3f3cd4c82353b667b4e4a3a88f0b03bb
EvilPluto <xhk6870@163.com>
2017-04-29 20:36:00 +0800

    修改了settingScene内容

--- MMXTH/SettingScene.m
+++ MMXTH/SettingScene.m
@@ -40,17 +40,26 @@
     [viewBg setScale:self.contentSize.width/viewBg.contentSize.width];
     [self addChild:viewBg z:1 name:@"pic"];
     
+    
+    CCSprite *littleBg =[CCSprite spriteWithImageNamed:@"暂停背景.png"];
+    littleBg.positionType=CCPositionTypeNormalized;
+    littleBg.position=ccp(0.5f, 0.45f);
+    [littleBg setScale:0.6*self.contentSize.width/littleBg.contentSize.width];
+    [self addChild:littleBg z:2];
+    
     //音量开关文字
-    CCLabelTTF *lable = [CCLabelTTF labelWithString:@"背景音乐" fontName:@"TrebuchetMS-Bold" fontSize:20];
-    lable.positionType = CCPositionTypeNormalized;
-    lable.position = ccp(0.3f, 0.7f);
-    [self addChild:lable z:10];
+    CCLabelTTF *label = [CCLabelTTF labelWithString:@"背景音乐" fontName:@"TrebuchetMS-Bold" fontSize:20];
+    [label setColor:[CCColor colorWithRed:203.0/255.0 green:94.0/255.0 blue:23.0/255.0]];
+    label.positionType = CCPositionTypeNormalized;
+    label.position = ccp(0.35f, 0.7f);
+    [self addChild:label z:10];
     
     //音量大小文字
-    CCLabelTTF *lable1 = [CCLabelTTF labelWithString:@"音量" fontName:@"TrebuchetMS-Bold" fontSize:20];
-    lable1.positionType = CCPositionTypeNormalized;
-    lable1.position = ccp(0.3f, 0.45f);
-    [self addChild:lable1 z:10];
+    CCLabelTTF *label1 = [CCLabelTTF labelWithString:@"音量" fontName:@"TrebuchetMS-Bold" fontSize:20];
+    [label1 setColor:[CCColor colorWithRed:203.0/255.0 green:94.0/255.0 blue:23.0/255.0]];
+    label1.positionType = CCPositionTypeNormalized;
+    label1.position = ccp(0.35f, 0.45f);
+    [self addChild:label1 z:10];
     
     //滑块调节音量
     CCSpriteFrame *sliderRuler=[CCSpriteFrame frameWithImageNamed:@"ruler.png"];
@@ -59,18 +68,18 @@
     musicSlider.sliderValue=[AudioPlayer audioplayer].player.volume;
     [musicSlider setTarget:self selector:@selector(onMusicValChange:)];
     musicSlider.positionType=CCPositionTypeNormalized;
-    musicSlider.position=ccp(0.45f, 0.4f);
+    musicSlider.position=ccp(0.5f, 0.4f);
     [self addChild:musicSlider z:11 name:@"slider"];
     
     //开关控制音量开启
-    musicSwitch=[[UISwitch alloc] initWithFrame:CGRectMake(100, 50, 0, 0)];
+    musicSwitch=[[UISwitch alloc] initWithFrame:CGRectMake(0, 0, 500.f, 10.f)];
     [musicSwitch addTarget:self action:@selector(soundJudge:) forControlEvents:UIControlEventValueChanged];
-    musicSwitch.center=CGPointMake(280, 100);
+    [musicSwitch setCenter:ccp(0.55*self.contentSize.width, 0.3*self.contentSize.height)];
     
     [musicSwitch setOn:[AudioPlayer audioplayer].ifMusicOn];
     CCLOG(@"~~~~~~sw.on:%d",musicSwitch.on);
     CCLOG(@"~~~~~~sw.selected:%d",musicSwitch.selected);
-    [[[CCDirector sharedDirector] view]addSubview:musicSwitch];
+    [[[CCDirector sharedDirector] view] addSubview:musicSwitch];
     //release?
     
     
